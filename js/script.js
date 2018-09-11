131fd046d3789d9ac509cc87dcfbe205441a6656
zirg3 <zirg31@gmail.com>
2018-09-11 22:11:57 +0300

    доработки

--- js/script.js
+++ js/script.js
@@ -16,14 +16,16 @@ close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("modal-show");
 });
+
 window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode === 27) {
-      evt.preventDefault();
-      if (popup.classList.contains("modal-show")) {
-        popup.classList.remove("modal-show");
-      }
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
     }
-  });
+  }
+});
+
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("modal-show");
@@ -35,5 +37,6 @@ form.addEventListener("submit", function (evt) {
     evt.preventDefault();
     popup.classList.remove("modal-error");
     popup.offsetWidth = popup.offsetWidth;
-    popup.classList.add("modal-error"); }
+    popup.classList.add("modal-error");
+  }
 });
