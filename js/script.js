a3415926423afd7499996097b88de4d333b1fce8
zirg3 <zirg31@gmail.com>
2018-09-11 12:58:31 +0300

    доработки

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 var write = document.querySelector(".btn-write");
 var popup = document.querySelector(".popup");
+var close = document.querySelector(".modal-close");
+var form = popup.querySelector(".contact-form");
+var user_name = popup.querySelector("[name=user-name]");
+var email = popup.querySelector("[name=email]");
+
 
 write.addEventListener("click", function (evt) {
   evt.preventDefault();
@@ -7,9 +12,28 @@ write.addEventListener("click", function (evt) {
   popup.classList.add("modal-show");
 });
 
-var close = document.querySelector(".modal-close");
-
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("modal-show");
+});
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (popup.classList.contains("modal-show")) {
+        popup.classList.remove("modal-show");
+      }
+    }
+  });
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!user_name.value || !email.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error"); }
 });
